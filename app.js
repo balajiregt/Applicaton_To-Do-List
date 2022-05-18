44cd84929bd609219713fc0c4db9879e6c3f9155
BalajiK <71222979+balajiregt@users.noreply.github.com>
2022-05-18 10:35:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputfield');
+let DeleteAll=document.getElementById('Delete')
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+    //clicking the list item
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        paragraph.style.textDecoration = "none";
+    })
+
+   // paragraph.addEventListener('dblclick', function(){
+     //   toDoContainer.removeChild(paragraph);
+   // })
+    
+})
+
+Delete.addEventListener('click',()=>{
+    toDoContainer.remove()
+})
+
